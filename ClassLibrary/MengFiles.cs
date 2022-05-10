329077cb8eec987901e550a52db4fc96389bc414
00yb00 <92918077+00yb00@users.noreply.github.com>
2022-05-10 13:03:42 +0300

    Add project files.

---
+++ ClassLibrary/MengFiles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary
+{
+    public class MengFiles
+    {
+        string src = @"D:\learning new things\lehavaLessons\files\";
+         Task task1;
+         Random random = new Random();
+        public Task<string> CreateFile(int numberChars,int numberFiles)
+        {
+            string nameOfFile="";
+            Task<string>t = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < numberFiles; i++)
+                {               
+                    System.Threading.Thread.Sleep(1000);
+                    nameOfFile ="file"+ random.Next(1, 100000).ToString() + ".txt";
+                    System.IO.File.WriteAllText(src+ nameOfFile, string.Concat(Enumerable.Repeat("*", numberChars)));
+                }  return nameOfFile;
+            });
+            return t;
+        }
+        public void DeleteFile(string nameFile)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                System.IO.File.Delete(src+nameFile);
+            });
+            
+        }
+        public void CreateThreads()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+            CreateFile(10000000,10);
+            }
+        }
+        public async void CreateAndDelete()
+        {
+           string FileName= await CreateFile(30000000, 1);
+            DeleteFile(FileName);
+
+        }
+    }
+}
---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+namespace WindowsForms
+{ 
+
+    public partial class Form1 : Form
+    { 
+        public MengFiles m=new MengFiles();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            m.CreateFile(10000000, 10);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m.CreateThreads();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            m.CreateAndDelete();
+        }
+    }
+}
